8ec6801c1f091e8131f3279ad7800eb68bfc4511
oreon <singhjess@gmail.com>
2018-09-11 12:56:30 -0400

    tests and wizard

---
+++ restful.py
@@ -0,0 +1,52 @@
+#pip install flask flask-restful
+# test : curl --header "Content-Type: application/json"   --request POST   --data '{"firstName":"and","lastName":"and"}'   http://localhost:5050/rs/validatenames
+
+
+from flask import Flask,jsonify, request
+from flask_restful import Resource, Api, reqparse
+
+parser = reqparse.RequestParser()
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+
+    def options (self):
+        return {'Allow' : 'POST' }, 200, \
+               { 'Access-Control-Allow-Origin': '*', \
+                 'Access-Control-Allow-Methods' : 'PUT,GET' }
+
+    def get(self):
+        return {'hello': 'world'}
+
+    def post(self):
+        json_data = request.get_json(force=True)
+        print(json_data)
+        un = ""
+        pw = ""
+        try:
+            un = json_data[0]['firstName']
+            pw = json_data[0]['lastName']
+        except:
+            un = json_data[0]['businessName']
+
+        #if(un  == '' and pw == ''):  un = json_data[0]['businessName']
+
+        typeCd= "typeCd"
+
+        if(un  == 'and' or pw == 'and'): return ([{typeCd : 2},])
+        if( (un  == 'plumbing' or un =="tech") or (pw  == 'plumbing' or pw =="tech")  ):
+            return ([{typeCd : 1},])
+
+        if(un  == 'british' and pw == 'columbia'):
+            return ([{typeCd : 1},])
+
+
+        print("got "  + un)
+        return []
+
+api.add_resource(HelloWorld, '/rso/rs/validatenames')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
--- screen.py
+++ screen.py
@@ -4,11 +4,16 @@ cnt = 0
 
 while True:
     pyautogui.moveTo(100, 150)
+
     for i in range(1, 4):
         pyautogui.click(1050 + i*25, 773)
 
-    pyautogui.click(500, 300)
+    # x, y = pyautogui.locateCenterOnScreen('remote.png')
+    # pyautogui.click(x, y)
+    #pyautogui.click(x, y)
     pyautogui.screenshot('myscreen.png')
     cnt = cnt + 1
     print ("clciked " +  str(cnt))
+    for pos in pyautogui.locateAllOnScreen('remote.png'):
+        print(pos)
     time.sleep(60 * 7)
